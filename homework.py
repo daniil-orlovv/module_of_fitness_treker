3a28cda17128f6c8920fcd89dcfcb69d119161d8
Даниил Орлов <danilorlov9898@yandex.ru>
2023-01-07 16:55:46 +0300

    Homework

--- homework.py
+++ homework.py
@@ -64,7 +64,7 @@ class Running(Training):
     def get_spent_calories(self) -> float:
         return ((self.CALORIES_MEAN_SPEED_MULTIPLIER
                 * self.get_mean_speed() + self.CALORIES_MEAN_SPEED_SHIFT)
-                * self.weight / self.M_IN_KM * (self.duration * self.MIN_IN_H)
+                * self.weight / self.M_IN_KM * (self.duration * self.MIN_IN_H))
 
 
 class SportsWalking(Training):
